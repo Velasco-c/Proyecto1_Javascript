b4b623d8beccb2881d422db94be26070856608b8
Velasco-c <carlos.velasco.est@gmail.com>
2026-05-16 19:03:02 -0600

    feat: :sparkles: mejoras esteticas de visualizacion de contenido del usuario y cambio de boton de cerra sesion

--- javascript/perfil.js
+++ javascript/perfil.js
@@ -1,7 +1,19 @@
+const obtenerValor = (id) => document.getElementById(id)?.value.trim();
+const obtenerElemento = (id) => document.getElementById(id);
 document.addEventListener('DOMContentLoaded', () => {
-    const textoBienvenida = document.getElementById('Bienvenido');
+    const textoBienvenida = obtenerElemento('Bienvenido');
     const usuarioLogueado = localStorage.getItem('usuarioLogueado');
+    const usuario = obtenerElemento('user-name');
+    const email = obtenerElemento('user-email')
+    const datosUsuario = JSON.parse(localStorage.getItem('usuarios')) || [];
+    const datosEncontrado = datosUsuario.find(v => v.username === usuarioLogueado);
+    if (!datosEncontrado) {
+        alert('usario no encontrado');
+    }
+
     if (usuarioLogueado && textoBienvenida) {
         textoBienvenida.textContent = `${usuarioLogueado}`;
+        usuario.textContent = `${usuarioLogueado}`
+        email.textContent = `${datosEncontrado.email}`
     }
 });
